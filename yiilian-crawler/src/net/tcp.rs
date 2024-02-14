ff4e943c7e22c09d9f5ddb57fd1d2fe595cb16e8
evan <564374058@qq.com>
2024-02-14 19:39:18 +0800

    add handshake dump

--- yiilian-crawler/src/net/tcp.rs
+++ yiilian-crawler/src/net/tcp.rs
@@ -11,7 +11,8 @@ pub async fn read_all(stream: &mut TcpStream) -> Result<Bytes, Error> {
         match stream.read(&mut buf).await {
             Ok(0) => break,
             Ok(n) => {
-                println!("{n}");
+                // println!("{n}");
+                println!("{:?}", &buf[0..n]);
                 rst.extend(&buf[0..n]);
             }
             Err(e) => {
