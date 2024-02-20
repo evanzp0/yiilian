05cb65663cf702261beb70e9d1c9d2ad85ab13c2
evan <564374058@qq.com>
2024-02-20 20:52:58 +0800

    add log_segment

--- yiilian-mq/src/lib.rs
+++ yiilian-mq/src/lib.rs
@@ -1,1 +1,2 @@
 pub mod message;
+pub mod log_segment;
---
+++ yiilian-mq/src/log_segment.rs
@@ -0,0 +1,19 @@
+use std::path::PathBuf;
+
+use memmap::MmapMut;
+
+const CONSUMER_OFFSETS_FILE_NAME: &str = "__consumer_offsets";
+const LOG_DATA__FILE_EXTENSION: &str = ".log";
+const LOG_INDEX__FILE_EXTENSION: &str = ".index";
+const TIME_INDEX__FILE_EXTENSION: &str = ".timeindex";
+
+pub struct LogSegment {
+    length: usize,
+    message_count: usize,
+    offset: u64,
+    prefix_path: PathBuf,
+    log_data: MmapMut,
+    log_index: MmapMut,
+    time_index: MmapMut,
+    consumer_offsets: MmapMut,
+}
