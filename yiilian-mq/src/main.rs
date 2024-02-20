28301f0453ec9371d5c8718c8c023fe92a85f9aa
evan <564374058@qq.com>
2024-02-20 20:28:42 +0800

    update

--- yiilian-mq/src/main.rs
+++ yiilian-mq/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::fs::OpenOptions;
+use std::path::PathBuf;
+
+use memmap::MmapMut;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let path: PathBuf = "./test_file.txt".into();
+    let file = OpenOptions::new()
+                           .read(true)
+                           .write(true)
+                           .create(true)
+                           .open(&path)?;
+    file.set_len(5)?;
+    
+    let mut mmap = unsafe { MmapMut::map_mut(&file)? };
+    
+    mmap.copy_from_slice(b"12345");
+
+    println!("{:?}", &mmap[0..5]);
+
+    
+    Ok(())
 }
