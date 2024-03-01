d93aab72f1080cddbdb539f2b22369dda63763be
evan <564374058@qq.com>
2024-03-01 22:11:42 +0800

    update

--- yiilian-mq/src/topic.rs
+++ yiilian-mq/src/topic.rs
@@ -1,14 +1,23 @@
 use std::path::PathBuf;
 
+use crate::{consumer_offsets::ConsumerOffsets, segment::active_segment::ActiveSegment};
+
 
 pub struct Topic {
     name: String,
     path: PathBuf,
+    active_segment: ActiveSegment,
+    consumer_offsets: ConsumerOffsets,
+    segment_offsets: Vec<u64>,
 }
 
 impl Topic {
     pub fn new(name: String, mut base_path: PathBuf) -> Self {
         base_path.push(name.clone());
+
+        let active_segment = ActiveSegment::new(offset, base_path);
+
+
         Topic {
             name,
             path: base_path,
