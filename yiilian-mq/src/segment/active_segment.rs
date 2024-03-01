b853479be1bab5c0c1f6fd76cb8b44060042a503
evan <564374058@qq.com>
2024-03-01 11:14:06 +0800

    update

--- yiilian-mq/src/segment/active_segment.rs
+++ yiilian-mq/src/segment/active_segment.rs
@@ -3,11 +3,11 @@ use std::{fs::OpenOptions, path::PathBuf};
 use memmap::MmapMut;
 use yiilian_core::common::error::Error;
 
-use crate::segment::{
+use crate::{message::Message, segment::{
     calc_log_index_size, LOG_DATA_FILE_EXTENSION, LOG_DATA_SIZE, LOG_INDEX_FILE_EXTENSION,
-};
+}};
 
-use super::{log_data::LogData, log_index::LogIndex};
+use super::{log_data::LogData, log_index::{LogIndex, LogIndexItem}};
 
 pub struct ActiveSegment {
     offset: u64,
@@ -64,5 +64,13 @@ impl ActiveSegment {
             log_data,
             log_index,
         })
+    }
+
+    pub fn push_message(&mut self, message: Message) -> Result<(), Error> {
+        let index_item = LogIndexItem::new(message.offset(), self.log_data.len());
+        self.log_data.push(message)?;
+        self.log_index.push(index_item)?;
+
+        Ok(())
     }
 }
