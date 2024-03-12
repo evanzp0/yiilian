fbb9c34bb36395e802a1df9b3c621a815c800d30
evan <564374058@qq.com>
2024-03-12 11:51:27 +0800

    update

--- yiilian-mq/src/segment/active_segment.rs
+++ yiilian-mq/src/segment/active_segment.rs
@@ -103,7 +103,7 @@ impl ActiveSegment {
             //     self.offset()
             // }
 
-            println!("{:?}", self.log_index);
+            // println!("{:?}", self.log_index);
 
             index.message_offset() + 1
             
