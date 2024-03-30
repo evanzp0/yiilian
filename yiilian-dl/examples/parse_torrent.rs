fddd7bc27b8ec5bf38215923b8a52f94cb597ee3
evan <564374058@qq.com>
2024-03-30 22:24:00 +0800

    update

--- yiilian-dl/examples/parse_torrent.rs
+++ yiilian-dl/examples/parse_torrent.rs
@@ -4,7 +4,7 @@ use yiilian_dl::bt::data::bt_torrent::BtTorrent;
 
 fn main() {
 
-    let test_torrent = fs::read("/home/evan/workspace/yiilian/torrent/1.torrent").unwrap();
+    let test_torrent = fs::read("/home/evan/workspace/yiilian/torrent/3.torrent").unwrap();
 
     let test_torrent: BtTorrent = (&test_torrent[..]).try_into().unwrap();
 
