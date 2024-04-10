334dc3bbdb73bfd59ea9e145781516bec7810cb4
evan <564374058@qq.com>
2024-04-11 00:33:04 +0800

    udpate

--- yiilian-core/examples/working_dir.rs
+++ yiilian-core/examples/working_dir.rs
@@ -1,15 +1,10 @@
-use yiilian_core::common::{util::setup_log4rs_from_file, working_dir::WorkingDir};
+use yiilian_core::common::working_dir::WorkingDir;
 
 fn main() {
     let wd = WorkingDir::new();
     let rst = wd.get_path_by_entry("Cargo.toml");
 
-    println!("{:?}", rst);
     println!("{:#?}", wd.exec_pathes());
 
-    let log4rs_path = wd.get_path_by_entry("log4rs.yml");
-    println!("{:?}", log4rs_path);
-    setup_log4rs_from_file(&log4rs_path.unwrap());
-
-    log::error!("abcd");
+    println!("{:?}", rst);
 }
